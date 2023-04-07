7f6487e357e77e415a1c5a8b1a6ed363d700340c
emadhussein3@gmail.com <emadhussein3@gmail.com>
2023-04-07 22:37:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            InputSimulator simulator = new InputSimulator();
+
+            while (isRunning)
+            {
+                // Press Arrow Up
+                simulator.Keyboard.KeyPress(VirtualKeyCode.UP);
+                Thread.Sleep(100);
+
+                // Press CTRL + A
+                simulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_A);
+                Thread.Sleep(100);
+
+                // Press Delete
+                simulator.Keyboard.KeyPress(VirtualKeyCode.DELETE);
+
+                // Press Enter
+                simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                Thread.Sleep(100);
+
+                // Press Enter again
+                simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                Thread.Sleep(100);
+
+                // Press Enter again
+                simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+                Thread.Sleep(100);
+
+                // Press Backspace
+                simulator.Keyboard.KeyPress(VirtualKeyCode.BACK);
+
+                // Check if F2 is pressed to pause the loop
+                if (simulator.InputDeviceState.IsKeyDown(VirtualKeyCode.F2))
+                {
+                    isRunning = false
+                }
+            }
+        }
+    }
+}
